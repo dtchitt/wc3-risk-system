54f2ab23e220d3b0c1f48f3313f4b4593560e196
dtchitt <danielchittenden@yahoo.com>
2023-07-21 23:12:05 -0700

    add console changes

---
+++ src/app/ui/console.ts
@@ -0,0 +1,114 @@
+import { MAP_NAME, MAP_VERSION } from '../utils/map-info';
+
+export function SetConsoleUI() {
+	// Disable Resource Tooltips
+	const resourceFrame: framehandle = BlzGetFrameByName('ResourceBarFrame', 0);
+	BlzFrameSetVisible(BlzFrameGetChild(resourceFrame, 1), false); // lumber tooltip
+	BlzFrameSetVisible(BlzFrameGetChild(resourceFrame, 2), false); // upkeep tooltip
+	BlzFrameSetVisible(BlzFrameGetChild(resourceFrame, 3), false); // supply tooltip
+
+	// Reposition Resource Frames
+	const upkeepFrame: framehandle = BlzGetFrameByName('ResourceBarUpkeepText', 0);
+	BlzFrameSetAbsPoint(upkeepFrame, FRAMEPOINT_TOPRIGHT, 0.6485, 0.5972);
+	BlzFrameSetText(upkeepFrame, '');
+
+	const lumberFrame: framehandle = BlzGetFrameByName('ResourceBarLumberText', 0);
+	BlzFrameSetText(lumberFrame, '');
+	BlzFrameSetSize(lumberFrame, 0.0000001, 0.0000001);
+
+	const mapInfo: framehandle = BlzCreateFrameByType(
+		'TEXT',
+		'mapInfo',
+		BlzGetOriginFrame(ORIGIN_FRAME_GAME_UI, 0),
+		'EscMenuLabelTextTemplate',
+		0
+	);
+	BlzFrameSetPoint(mapInfo, FRAMEPOINT_LEFT, BlzGetFrameByName('ResourceBarSupplyText', 0), FRAMEPOINT_RIGHT, 0.035, 0.0);
+	BlzFrameSetTextAlignment(mapInfo, TEXT_JUSTIFY_CENTER, TEXT_JUSTIFY_RIGHT);
+	BlzFrameSetLevel(mapInfo, 2);
+	BlzFrameSetText(mapInfo, `v${MAP_VERSION}`);
+
+	const newTitle: string = 'discord.me/risk';
+	const newResourceHeader: string = 'www.youtube.com/@riskreforged3107';
+
+	BlzFrameSetText(BlzGetFrameByName('AllianceTitle', 0), newTitle);
+	BlzFrameSetText(BlzGetFrameByName('ResourceTradingTitle', 0), newResourceHeader);
+
+	BlzFrameSetVisible(BlzGetFrameByName('VisionHeader', 0), false);
+	BlzFrameSetVisible(BlzGetFrameByName('LumberHeader', 0), false);
+	BlzFrameSetVisible(BlzGetFrameByName('AlliedVictoryLabel', 0), false);
+	BlzFrameSetVisible(BlzGetFrameByName('AlliedVictoryCheckBox', 0), false);
+
+	for (let i = 0; i < 23; i++) {
+		BlzFrameSetVisible(BlzGetFrameByName('LumberBackdrop', i), false);
+		BlzFrameSetVisible(BlzGetFrameByName('LumberText', i), false);
+		BlzFrameSetVisible(BlzGetFrameByName('VisionCheckBox', i), false);
+	}
+
+	if (GetHandleId(BlzGetFrameByName('ChatPlayerLabel', 0)) == 0) {
+		Location(0, 0);
+	} else {
+		BlzFrameSetVisible(BlzGetFrameByName('ChatPlayerLabel', 0), false);
+	}
+
+	if (GetHandleId(BlzGetFrameByName('ChatPlayerRadioButton', 0)) == 0) {
+		Location(0, 0);
+	} else {
+		BlzFrameSetVisible(BlzGetFrameByName('ChatPlayerRadioButton', 0), false);
+	}
+
+	if (GetHandleId(BlzGetFrameByName('ChatPlayerMenu', 0)) == 0) {
+		Location(0, 0);
+	} else {
+		BlzFrameSetVisible(BlzGetFrameByName('ChatPlayerMenu', 0), false);
+	}
+
+	BlzCreateFrame('PlayerMessageFrame', BlzGetOriginFrame(ORIGIN_FRAME_GAME_UI, 0), 0, 0);
+	BlzCreateFrame('ErrorMessageFrame', BlzGetOriginFrame(ORIGIN_FRAME_GAME_UI, 0), 0, 0);
+	// hideUI(true);
+}
+
+export function hideUI(hidden: boolean) {
+	BlzHideOriginFrames(hidden);
+	BlzFrameSetVisible(BlzGetFrameByName('ConsoleUIBackdrop', 0), !hidden);
+	BlzFrameSetVisible(BlzGetFrameByName('UpperButtonBarFrame', 0), !hidden);
+
+	BlzEnableSelections(!hidden, !hidden);
+}
+
+export function ffaSetup() {
+	let AllyMenuTitle: framehandle = BlzGetFrameByName('AllianceTitle', 0);
+	let tempText: string = 'discord.me/risk';
+	tempText += '|n|n|cffffcc00Commands:|r';
+	tempText += '|n|cffffffff-cam #### (1000 min, 8500 max)';
+	tempText += '|n|cffffffff-def (resets cam to default)';
+	tempText += '|n|cffffffff-sb 1 OR -sb 2 (changes scoreboard)';
+	tempText += '|n|cffffffff-forfeit OR -ff (forfeits the game without leaving it)';
+	tempText += '|n|cffffffff-restart OR -ng (restarts the game if its over)';
+	tempText += '|n|cffffffff-stfu <player name> (mutes a player for 300 seconds)';
+	tempText += '|n|n|cffffcc00Hotkeys:|r';
+	tempText += '|n|cffffffff F1 (selects player tools)';
+	tempText += '|n|cffffffff F2 (cycles scoreboard)';
+	tempText += '|n|cffffffff F8 (cycles owned spawners)';
+
+	BlzFrameSetText(AllyMenuTitle, tempText);
+
+	BlzFrameSetVisible(BlzGetFrameByName('UnitsHeader', 0), false);
+	BlzFrameSetVisible(BlzGetFrameByName('AllyHeader', 0), false);
+	BlzFrameSetVisible(BlzGetFrameByName('GoldHeader', 0), false);
+	BlzFrameSetVisible(BlzGetFrameByName('AllianceDialogScrollBar', 0), false);
+	BlzFrameSetVisible(BlzGetFrameByName('AllianceAcceptButton', 0), false);
+	BlzFrameSetVisible(BlzGetFrameByName('AllianceAcceptButtonText', 0), false);
+	BlzFrameSetVisible(BlzGetFrameByName('PlayersHeader', 0), false);
+
+	for (let i = 0; i < 23; i++) {
+		BlzFrameSetVisible(BlzGetFrameByName('AllianceSlot', i), false);
+		BlzFrameSetVisible(BlzGetFrameByName('PlayerNameLabel', i), false);
+		BlzFrameSetVisible(BlzGetFrameByName('ColorBackdrop', i), false);
+		BlzFrameSetVisible(BlzGetFrameByName('ColorBorder', i), false);
+		BlzFrameSetVisible(BlzGetFrameByName('AllyCheckBox', i), false);
+		BlzFrameSetVisible(BlzGetFrameByName('GoldBackdrop', i), false);
+		BlzFrameSetVisible(BlzGetFrameByName('GoldText', i), false);
+		BlzFrameSetVisible(BlzGetFrameByName('UnitsCheckBox', i), false);
+	}
+}
