f15299e2b265ee7b69eae3a88dd49e6bd5e9ee7f
dtchitt <danielchittenden@yahoo.com>
2023-08-07 20:42:09 -0700

    prevent dead/left -> alive

--- src/app/player/status/strategies/alive-strategy.ts
+++ src/app/player/status/strategies/alive-strategy.ts
@@ -4,7 +4,7 @@ import { StatusStrategy } from './status-strategy';
 
 export class AliveStrategy implements StatusStrategy {
 	run(gamePlayer: ActivePlayer): void {
-		if (GetPlayerSlotState(gamePlayer.getPlayer()) == PLAYER_SLOT_STATE_LEFT) return;
+		if (GetPlayerSlotState(gamePlayer.getPlayer()) == PLAYER_SLOT_STATE_LEFT) return gamePlayer.status.set(PLAYER_STATUS.LEFT);
 
 		gamePlayer.status.status = PLAYER_STATUS.ALIVE;
 		gamePlayer.trackedData.income.income = 4;
