97c4b4a5923c12666a3c4d1ae640cca18ad8e666
dtchitt <danielchittenden@yahoo.com>
2023-08-08 09:53:49 -0700

    refactor distro service to be more efficient

--- src/app/game/services/distribution-service.ts
+++ src/app/game/services/distribution-service.ts
@@ -20,7 +20,6 @@ export class DistributionService {
 		const countryData: MultiMap<Country, ActivePlayer, number> = new MultiMap<Country, ActivePlayer, number>();
 
 		this.cityMax = Math.min(Math.floor(cities.length / players.length), 20);
-		ShuffleArray(cities);
 		ShuffleArray(players);
 
 		try {
@@ -29,9 +28,7 @@ export class DistributionService {
 				let player: ActivePlayer = players.pop();
 				let country: Country = CityToCountry.get(city);
 
-				if (!countryData.has(country, player)) countryData.set(country, player, 0);
-
-				if (countryData.get(country, player) < Math.floor(country.getCities().length / 2)) {
+				if (this.canChangeCityOwner(countryData, country, player)) {
 					this.changeCityOwner(city, player, countryData);
 				} else {
 					for (let counter = 0; counter <= 50; counter++) {
@@ -39,9 +36,7 @@ export class DistributionService {
 						city = this.getRandomCity(cities);
 						country = CityToCountry.get(city);
 
-						if (!countryData.has(country, player)) countryData.set(country, player, 0);
-
-						if (countryData.get(country, player) < Math.floor(country.getCities().length / 2) || city == null) {
+						if (this.canChangeCityOwner(countryData, country, player)) {
 							break;
 						}
 					}
@@ -60,10 +55,14 @@ export class DistributionService {
 
 	private getRandomCity(cities: City[]): City {
 		const randomIndex = Math.floor(Math.random() * cities.length);
-		let city: City = cities.splice(randomIndex, 1)[0];
+		const lastIndex = cities.length - 1;
 
-		if (CityToCountry.get(city).getCities().length == 1) {
-			city = this.getRandomCity(cities);
+		[cities[randomIndex], cities[lastIndex]] = [cities[lastIndex], cities[randomIndex]];
+
+		const city = cities.pop();
+
+		if (CityToCountry.get(city).getCities().length === 1) {
+			return this.getRandomCity(cities);
 		}
 
 		return city;
@@ -72,10 +71,16 @@ export class DistributionService {
 	private changeCityOwner(city: City, player: ActivePlayer, countryData: MultiMap<Country, ActivePlayer, number>) {
 		const country: Country = CityToCountry.get(city);
 
-		if (countryData.get(country, player) < Math.floor(country.getCities().length / 2)) {
+		if (this.canChangeCityOwner(countryData, country, player)) {
 			city.setOwner(player.getPlayer());
 			SetUnitOwner(city.guard.unit, player.getPlayer(), true);
 			countryData.set(country, player, countryData.get(country, player) + 1);
 		}
+	}
+
+	private canChangeCityOwner(countryData: MultiMap<Country, ActivePlayer, number>, country: Country, player: ActivePlayer) {
+		if (!countryData.has(country, player)) countryData.set(country, player, 0);
+
+		return countryData.get(country, player) < Math.floor(country.getCities().length / 2);
 	}
 }
