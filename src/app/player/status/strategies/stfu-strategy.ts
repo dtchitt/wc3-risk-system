9f2ac8d1359565208da980b087e76417f0be4775
dtchitt <danielchittenden@yahoo.com>
2023-08-07 20:41:37 -0700

    fix status updating during stfu

--- src/app/player/status/strategies/stfu-strategy.ts
+++ src/app/player/status/strategies/stfu-strategy.ts
@@ -5,10 +5,8 @@ import { StatusStrategy } from './status-strategy';
 export class STFUStrategy implements StatusStrategy {
 	run(gamePlayer: ActivePlayer): void {
 		if (gamePlayer.status.isNomad() || gamePlayer.status.isAlive() || gamePlayer.status.isSTFU()) return;
-
 		const oldStatus = gamePlayer.status.status;
 		gamePlayer.status.status = PLAYER_STATUS.STFU;
-
 		SetPlayerState(gamePlayer.getPlayer(), PLAYER_STATE_OBSERVER, 1);
 
 		gamePlayer.status.statusDuration = 300;
@@ -16,7 +14,11 @@ export class STFUStrategy implements StatusStrategy {
 		const tick: number = 1;
 
 		TimerStart(muteTimer, tick, true, () => {
-			if (gamePlayer.status.statusDuration <= 0) {
+			if (GetPlayerSlotState(gamePlayer.getPlayer()) == PLAYER_SLOT_STATE_LEFT) {
+				gamePlayer.status.set(PLAYER_STATUS.LEFT);
+				PauseTimer(muteTimer);
+				DestroyTimer(muteTimer);
+			} else if (gamePlayer.status.statusDuration <= 0) {
 				SetPlayerState(gamePlayer.getPlayer(), PLAYER_STATE_OBSERVER, 0);
 				gamePlayer.status.status = oldStatus;
 				PauseTimer(muteTimer);
