ad3789279e32182d2b512eeef1cde37857172db2
dtchitt <danielchittenden@yahoo.com>
2023-08-24 23:31:38 -0700

    anti spam rework

--- src/app/triggers/anti-spam.ts
+++ src/app/triggers/anti-spam.ts
@@ -1,18 +1,21 @@
 import { GameManager } from '../game/game-manager';
+import { TimedEventManager } from '../libs/timer/timed-event-manager';
 import { PlayerManager } from '../player/player-manager';
 import { PLAYER_STATUS } from '../player/status/status-enum';
 import { ActivePlayer } from '../player/types/active-player';
 import { PLAYER_SLOTS } from '../utils/utils';
 
 type AntiSpamData = {
-	timer: timer;
 	string: string;
 	count: number;
 };
 
+const SPAM_MAP: Map<player, AntiSpamData> = new Map<player, AntiSpamData>();
+const SPAM_THRESHOLD: number = 3;
+const MINIMUM_MESSAGE_LENGTH: number = 21;
+const SPAM_TIMER_DURATION: number = 5;
+
 export const AntiSpam = () => {
-	const spamMap: Map<player, AntiSpamData> = new Map<player, AntiSpamData>();
-	const threshold: number = 3;
 	const t: trigger = CreateTrigger();
 
 	for (let i = 0; i < PLAYER_SLOTS; i++) {
@@ -22,61 +25,38 @@ export const AntiSpam = () => {
 	TriggerAddCondition(
 		t,
 		Condition(() => {
+			if (GetEventPlayerChatString().length < MINIMUM_MESSAGE_LENGTH) return false;
+			if (!GameManager.getInstance().isStateMetaGame()) return false;
+
 			const player: player = GetTriggerPlayer();
 
-			if (spamMap.has(player)) {
-				if (
-					GetEventPlayerChatString().includes(spamMap.get(player).string) &&
-					GetEventPlayerChatString().length >= 21 &&
-					spamMap.get(player).string.length >= 21
-				) {
-					spamMap.get(player).count++;
-					if (onSpamDetection(spamMap, player, threshold)) {
-						spamMap.delete(player);
-						PauseTimer(spamMap.get(player).timer);
-						DestroyTimer(spamMap.get(player).timer);
-					}
-				}
-			} else {
-				spamMap.set(player, <AntiSpamData>{
-					timer: CreateTimer(),
+			if (!SPAM_MAP.has(player)) {
+				SPAM_MAP.set(player, <AntiSpamData>{
 					string: GetEventPlayerChatString(),
 					count: 1,
 				});
 
-				let duration: number = 5;
-				let tick: number = 0.03;
-				let timer: timer = spamMap.get(player).timer;
+				const timedEventManager: TimedEventManager = TimedEventManager.getInstance();
 
-				TimerStart(timer, tick, true, () => {
-					if (duration <= 0) {
-						onSpamDetection(spamMap, player, threshold);
+				const timedEvent = timedEventManager.registerTimedEvent(SPAM_TIMER_DURATION, () => {
+					if (SPAM_MAP.get(player).count >= SPAM_THRESHOLD) {
+						const gPlayer: ActivePlayer = PlayerManager.getInstance().players.get(player);
 
-						spamMap.delete(player);
-						PauseTimer(spamMap.get(player).timer);
-						DestroyTimer(spamMap.get(player).timer);
+						gPlayer.status.set(PLAYER_STATUS.FORFEIT);
+						SetPlayerState(gPlayer.getPlayer(), PLAYER_STATE_OBSERVER, 1);
+
+						timedEvent.duration = -1;
 					}
 
-					duration -= tick;
+					if (timedEvent.duration <= 1) {
+						SPAM_MAP.delete(player);
+					}
 				});
+			} else if (GetEventPlayerChatString().includes(SPAM_MAP.get(player).string)) {
+				SPAM_MAP.get(player).count++;
 			}
 
-			return true;
+			return false;
 		})
 	);
 };
-
-function onSpamDetection(spamMap: Map<player, AntiSpamData>, player: player, threshold: number): boolean {
-	if (!GameManager.getInstance().isStateMetaGame()) return false;
-
-	if (spamMap.get(player).count >= threshold) {
-		const gPlayer: ActivePlayer = PlayerManager.getInstance().players.get(player);
-
-		gPlayer.status.set(PLAYER_STATUS.FORFEIT);
-		SetPlayerState(gPlayer.getPlayer(), PLAYER_STATE_OBSERVER, 1);
-
-		return true;
-	}
-
-	return false;
-}
