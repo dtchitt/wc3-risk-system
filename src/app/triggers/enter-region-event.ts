73cb72d49848ce6496dee95fa46ce82e63439ecf
dtchitt <danielchittenden@yahoo.com>
2023-09-04 11:40:03 -0700

    update enter region

--- src/app/triggers/enter-region-event.ts
+++ src/app/triggers/enter-region-event.ts
@@ -2,13 +2,11 @@ import { CityRegionSize } from 'src/configs/city-settings';
 import { City } from '../city/city';
 import { RegionToCity, UnitToCity } from '../city/city-map';
 import { UNIT_TYPE } from '../utils/unit-types';
-import { DistanceBetweenCoords } from '../utils/utils';
 import { GetUnitsInRangeByAllegiance } from '../utils/guard-filters';
 import { CompareUnitByValue } from '../utils/unit-comparisons';
 import { UNIT_ID } from 'src/configs/unit-id';
 
 export const EnterRegionTrigger: trigger = CreateTrigger();
-const guardDistanceAllowed: number = CityRegionSize / 2 + 50;
 
 export function EnterRegionEvent() {
 	TriggerAddCondition(
@@ -18,7 +16,10 @@ export function EnterRegionEvent() {
 
 			const city: City = RegionToCity.get(GetTriggeringRegion());
 
-			if (checkForGuard(city, city.guard.unit)) return true;
+			//Validate current guard
+			if (IsUnitInRegion(GetTriggeringRegion(), city.guard.unit) && city.isValidGuard(city.guard.unit)) {
+				return true;
+			}
 
 			let g: group = CreateGroup();
 
@@ -66,15 +67,4 @@ function getGuardChoice(g: group, guardChoice: unit, city: City) {
 	});
 
 	return guardChoice;
-}
-
-function checkForGuard(city: City, guard: unit): boolean {
-	if (
-		city.isValidGuard(guard) &&
-		DistanceBetweenCoords(GetUnitX(guard), GetUnitY(guard), city.guard.defaultX, city.guard.defaultY) < guardDistanceAllowed
-	) {
-		return true;
-	}
-
-	return false;
 }
