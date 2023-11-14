c8fa69d71a4c2ac607ee2c44d45a323d7e676abd
dtchitt <danielchittenden@yahoo.com>
2023-11-13 18:01:20 -0800

    1.94 release

--- src/app/utils/map-info.ts
+++ src/app/utils/map-info.ts
@@ -1,5 +1,5 @@
 
 	//Do not edit - this will automatically update based on the project config.json upon building the map
 	export const MAP_NAME: string = 'Risk Europe';
-	export const MAP_VERSION: string = '1.93';
+	export const MAP_VERSION: string = '1.94';
   
