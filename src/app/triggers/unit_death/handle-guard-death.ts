761cd7d5983347a544c26b9be8b32ad439fdca72
dtchitt <danielchittenden@yahoo.com>
2023-12-30 21:21:51 -0800

    invalid guard handling

--- src/app/triggers/unit_death/handle-guard-death.ts
+++ src/app/triggers/unit_death/handle-guard-death.ts
@@ -6,17 +6,6 @@ import { InvalidGuardHandler } from './invalid-guard-handler';
 import { AlliedKillHandler } from './allied-kill-handler';
 import { EnemyKillHandler } from './enemy-kill-handler';
 
-//Where should I handle ships?
-//Need to check if killer is ship and city is port or land city
-
-//Step 1. Handle owned/allied guard killers - check in large radius for either
-//Step 2. Handle enemy killer
-//Step 2a. Check if there is owned/allied valid units in small radius
-//Step 2b. if 2a is false, find valid enemy unit of guard in large radius
-//Step 3. Handle null guardchoice - this happens when no valid replacements are found
-//Step 3a. if tower is killer, do not change owner, create dummy for owner
-//Step 3b. if killer is owned/allied but dead, create dummy for owner
-//Step 3c. if killer is enemy but dead, create dummy for killer and change owner
 export function HandleGuardDeath(dyingUnit: unit, killingUnit: unit) {
 	const city: LandCity | PortCity = UnitToCity.get(dyingUnit);
 
@@ -33,6 +22,6 @@ export function HandleGuardDeath(dyingUnit: unit, killingUnit: unit) {
 
 	//Handle cases where no valid guard is found
 	if (!guardChoice) {
-		InvalidGuardHandler(dyingUnit, killingUnit);
+		InvalidGuardHandler(city, killingUnit);
 	}
 }
