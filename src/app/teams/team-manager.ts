25f96016f926dd9c07dd3ff11b6931fb33dfd81b
dtchitt <danielchittenden@yahoo.com>
2024-01-20 17:23:52 -0800

    add team manager

---
+++ src/app/teams/team-manager.ts
@@ -0,0 +1,84 @@
+import { PLAYER_SLOTS } from '../utils/utils';
+import { Team } from './team';
+
+export class TeamManager {
+	private teams: Map<number, Team>;
+	private static instance: TeamManager;
+
+	private constructor() {
+		this.teams = new Map<number, Team>();
+		const teams: Map<number, player[]> = new Map<number, player[]>();
+
+		for (let i = 0; i < PLAYER_SLOTS; i++) {
+			const player = Player(i);
+
+			if (!IsPlayerSlotState(player, PLAYER_SLOT_STATE_PLAYING) || IsPlayerObserver(player)) {
+				continue;
+			}
+
+			const teamNumber = GetPlayerTeam(player) + 1;
+
+			if (!teams.has(teamNumber)) {
+				teams.set(teamNumber, [player]);
+			} else {
+				teams.get(teamNumber).push(player);
+			}
+		}
+
+		teams.forEach((players, teamNumber) => {
+			if (!this.teams.has(teamNumber)) {
+				const alliance = new Team(players);
+
+				this.teams.set(teamNumber, alliance);
+			}
+		});
+	}
+
+	public static getInstance(): TeamManager {
+		if (TeamManager.instance == null) {
+			this.instance = new TeamManager();
+		}
+
+		return this.instance;
+	}
+
+	public getTeams(): Team[] {
+		return [...this.teams.values()];
+	}
+
+	public getTeamFromNumber(teamNum: number): Team {
+		return this.teams.get(teamNum + 1);
+	}
+
+	public getTeamFromPlayer(player: player): Team {
+		return this.teams.get(GetPlayerTeam(player) + 1);
+	}
+
+	public static breakTeams() {
+		for (let i = 0; i < PLAYER_SLOTS; i++) {
+			const playerA: player = Player(i);
+
+			for (let j = 0; j < PLAYER_SLOTS; j++) {
+				const playerB: player = Player(j);
+
+				SetPlayerAlliance(playerA, playerB, ALLIANCE_PASSIVE, false);
+				SetPlayerAlliance(playerA, playerB, ALLIANCE_HELP_REQUEST, false);
+				SetPlayerAlliance(playerA, playerB, ALLIANCE_HELP_RESPONSE, false);
+				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_XP, false);
+				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_SPELLS, false);
+				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_VISION, false);
+				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_CONTROL, false);
+				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_ADVANCED_CONTROL, false);
+
+				SetPlayerAlliance(playerB, playerA, ALLIANCE_PASSIVE, false);
+				SetPlayerAlliance(playerB, playerA, ALLIANCE_HELP_REQUEST, false);
+				SetPlayerAlliance(playerB, playerA, ALLIANCE_HELP_RESPONSE, false);
+				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_XP, false);
+				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_SPELLS, false);
+				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_VISION, false);
+				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_CONTROL, false);
+				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_ADVANCED_CONTROL, false);
+			}
+		}
+	}
+}
---
+++ src/app/teams/team.ts
@@ -0,0 +1,61 @@
+import { STARTING_INCOME } from 'src/configs/game-settings';
+
+export class Team {
+	private teamNumber: number;
+	private teamMembers: player[];
+	private income: number;
+	private cities: number;
+	private kills: number;
+	private deaths: number;
+
+	public constructor(players: player[]) {
+		this.teamNumber = GetPlayerTeam(players[0]) + 1;
+		this.teamMembers = [];
+
+		players.forEach((player) => {
+			this.teamMembers.push(player);
+		});
+
+		this.income = players.length * STARTING_INCOME;
+	}
+
+	public getNumber() {
+		return this.teamNumber;
+	}
+
+	public getMembers() {
+		return this.teamMembers;
+	}
+
+	public updateIncome(delta: number) {
+		this.income += delta;
+	}
+
+	public updateCityCount(delta: number) {
+		this.cities += delta;
+	}
+
+	public updateKillCount(delta: number) {
+		this.kills += delta;
+	}
+
+	public updateDeathCount(delta: number) {
+		this.deaths += delta;
+	}
+
+	public getIncome() {
+		return this.income;
+	}
+
+	public getCities() {
+		return this.cities;
+	}
+
+	public getKills() {
+		return this.kills;
+	}
+
+	public getDeaths() {
+		return this.deaths;
+	}
+}
