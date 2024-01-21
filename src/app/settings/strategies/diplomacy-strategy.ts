5558f15c8e4348520eef171c6cae9fbdde388aa0
dtchitt <danielchittenden@yahoo.com>
2024-01-20 17:24:03 -0800

    update diplo strat

--- src/app/settings/strategies/diplomacy-strategy.ts
+++ src/app/settings/strategies/diplomacy-strategy.ts
@@ -1,7 +1,7 @@
+import { TeamManager } from 'src/app/teams/team-manager';
 import { SettingsStrategy } from './settings-strategy';
 import { AllyMenuFFASetup } from 'src/app/ui/console';
 import { HexColors } from 'src/app/utils/hex-colors';
-import { PLAYER_SLOTS } from 'src/app/utils/utils';
 
 export interface DiplomacyOptions {
 	option: number;
@@ -36,40 +36,12 @@ export class DiplomacyStrategy implements SettingsStrategy {
 	}
 
 	private handleFFA(): void {
-		//TODO This is temporary until I fix alliance manager
-		for (let i = 0; i < PLAYER_SLOTS; i++) {
-			const playerA: player = Player(i);
-
-			for (let j = 0; j < PLAYER_SLOTS; j++) {
-				const playerB: player = Player(j);
-
-				SetPlayerAlliance(playerA, playerB, ALLIANCE_PASSIVE, false);
-				SetPlayerAlliance(playerA, playerB, ALLIANCE_HELP_REQUEST, false);
-				SetPlayerAlliance(playerA, playerB, ALLIANCE_HELP_RESPONSE, false);
-				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_XP, false);
-				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_SPELLS, false);
-				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_VISION, false);
-				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_CONTROL, false);
-				SetPlayerAlliance(playerA, playerB, ALLIANCE_SHARED_ADVANCED_CONTROL, false);
-
-				SetPlayerAlliance(playerB, playerA, ALLIANCE_PASSIVE, false);
-				SetPlayerAlliance(playerB, playerA, ALLIANCE_HELP_REQUEST, false);
-				SetPlayerAlliance(playerB, playerA, ALLIANCE_HELP_RESPONSE, false);
-				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_XP, false);
-				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_SPELLS, false);
-				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_VISION, false);
-				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_CONTROL, false);
-				SetPlayerAlliance(playerB, playerA, ALLIANCE_SHARED_ADVANCED_CONTROL, false);
-			}
-		}
-
-		//AllianceManager.getInstance().clearAlliances();
+		TeamManager.breakTeams();
 		AllyMenuFFASetup();
 	}
 
 	private handleLobbyTeams(): void {
-		//TODO
-		//I could maybe control scoreboard creation from this strategy.
+		TeamManager.getInstance();
 	}
 
 	private handleRandomTeams(): void {
