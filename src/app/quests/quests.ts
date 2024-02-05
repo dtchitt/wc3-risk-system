d966943b550bd0b73dc5432e47141c3792c94c6a
dtchitt <danielchittenden@yahoo.com>
2024-02-04 20:00:57 -0800

    add tut and army comp quests

--- src/app/quests/quests.ts
+++ src/app/quests/quests.ts
@@ -5,6 +5,7 @@ export class Quests {
 	public static Create() {
 		Quests.Credits();
 		Quests.Tutorial();
+		Quests.ArmyComposition();
 	}
 
 	private static Credits() {
@@ -15,12 +16,11 @@ export class Quests {
 		description += '\nSS Ship Model: ??, please let ForLolz know if you know the author';
 		description += '\nSpecial Thanks: The Risk Community, Priwin, PsycoMarauder, RodOfNod, goble-r1sk, Saran, and all the devs before me!';
 
-		Quests.BuildQuest('Credits', description, 'ReplaceableTexturesWorldEditUIEditor-MultipleUnits.blp', false);
+		Quests.BuildQuest('Credits', description, 'ReplaceableTexturesCommandButtonsBTNBansheeMaster.blp', false);
 	}
 
 	private static Tutorial() {
-		let description: string = 'How to play:';
-		description += '\nThe goal of the game is to conquer 135 cities and hold them until the end of the turn.';
+		let description: string = 'The goal of the game is to conquer 135 cities and hold them until the end of the turn.';
 		description += '\nTo gain income you need to control a whole country when the turn ends.';
 		description += '\nIt is best to start with smaller countries to gain income quickly.';
 		description += '\nTry to expand in a way that will keep your countries connected.';
@@ -28,7 +28,24 @@ export class Quests {
 		description += '\nChat is essential in Risk, make sure to use it and read it. Diplomacy is key.';
 		description += '\nMake sure to peace other players, but also be ready to be backstabbed when your are vulnerable.';
 
-		Quests.BuildQuest('Tutorial', description, 'ReplaceableTexturesWorldEditUIEditor-MultipleUnits.blp', true);
+		Quests.BuildQuest('How to play', description, 'ReplaceableTexturesWorldEditUIEditor-Random-Unit.blp', true);
+	}
+
+	private static ArmyComposition() {
+		let description: string = 'Risk is not your typical, "buy the more expensive unit" game. Army composition is very important.';
+		description += ' The main bulk of your army will be Riflemen. They should be supported by Priests and Mortors.';
+		description += ' Those three unit types will do most of your fighting on land.';
+		description += ' It is also important to build a couple of Roarers to cast Roar and Dispel.';
+		description += ' Units such as Knights and Generals are mostly used to overhwelm your enemy in specific situations.';
+		description += ' These situations are not often, it is mostly when your enemy loses their Riflemen line.';
+		description += ' Artillery are a long range splash damage units. Be careful with them as they are defensively weak.';
+		description += ' Tanks are strong units that are best used in mass. Be sure to micro weak tanks back so they can heal.';
+		description += ' SS are king of the seas, they are the go to ship in FFA games.';
+		description += ' Warship B is best used to chase down other weak ships including SS.';
+		description += ' Warship A is an early game unit.';
+		description += ' It should only really be used the first couple of turns in fights on specific coastlines.';
+
+		Quests.BuildQuest('Army Composition', description, 'ReplaceableTexturesWorldEditUIEditor-MultipleUnits.blp', true);
 	}
 
 	private static BuildQuest(title: string, description: string, icon: string, required: boolean) {
