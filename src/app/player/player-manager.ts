ec84e04313752fb6abc868de8b7bbfa62267a2d2
Daniel Chittenden <danielchittenden@yahoo.com>
2024-08-10 23:09:49 -0700

    update banlist

--- src/app/player/player-manager.ts
+++ src/app/player/player-manager.ts
@@ -9,7 +9,7 @@ const banList: string[] = [
 	'nappa#11822', //Full screen spam
 	'keen13#2151', //Map saboteur
 	'arfan#11830', //Fake account to impersonate arfan
-	//'selinace#1683', //Multi accounter - serenity
+	'selinace#1683', //Multi accounter - serenity
 	'serenity#13183', //Multi accounter - selinace
 ];
 
