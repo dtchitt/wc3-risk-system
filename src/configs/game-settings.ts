6099ecc2bc5a4d51a031859f0caae7e3b557fc74
Microhive <microhive@outlook.com>
2024-11-26 23:44:06 +0100

    Default game settings

--- src/configs/game-settings.ts
+++ src/configs/game-settings.ts
@@ -1,12 +1,12 @@
 //Used to control how many of total cities you need.
 //This is a percentage of the total cities .6 = 60%
-export const CITIES_TO_WIN_MULTIPLIER: number = 0.13;
+export const CITIES_TO_WIN_MULTIPLIER: number = 0.6;
 
 //This is the starting gold for each player. 4 by default.
 export const STARTING_INCOME: number = 4;
 
 //This is the duration of a turn in seconds. 60 by default.
-export const TURN_DURATION_SECONDS: number = 5;
+export const TURN_DURATION_SECONDS: number = 60;
 
 //This is the modifier for the reduced win requirement. 0.01 is approximately 2 cities.
 export const OVERTIME_MODIFIER: number = 0.01;
