56a8b64935a064e703cd40f04f23aed8f7babbb1
Microhive <microhive@outlook.com>
2024-12-30 23:22:57 +0100

    Updating version

--- src/app/utils/map-info.ts
+++ src/app/utils/map-info.ts
@@ -1,4 +1,3 @@
-	//Do not edit - this will automatically update based on the project config.json upon building the map
-	export const MAP_NAME: string = 'Risk Europe';
-	export const MAP_VERSION: string = '2.06a';
-  
+//Do not edit - this will automatically update based on the project config.json upon building the map
+export const MAP_NAME: string = 'Risk Europe';
+export const MAP_VERSION: string = '2.06b';
