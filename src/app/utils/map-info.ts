c88879e49970029a6350110dcac073c8e1fc5138
Microhive <microhive@outlook.com>
2025-01-24 20:58:24 +0100

    Updated build pipeline

--- src/app/utils/map-info.ts
+++ src/app/utils/map-info.ts
@@ -1,5 +1,5 @@
 
 	//Do not edit - this will automatically update based on the project config.json upon building the map
 	export const MAP_NAME: string = 'Risk Europe';
-	export const MAP_VERSION: string = '2.07c';
+	export const MAP_VERSION: string = 'v2.6.2-alpha.1';
   
