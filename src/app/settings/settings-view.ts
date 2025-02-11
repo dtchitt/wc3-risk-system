b1123b177c7b71af10a3ef5842f7d5ba0b1b5bfb
Microhive <microhive@outlook.com>
2025-02-11 22:17:56 +0100

    Overtime is now by default 30

--- src/app/settings/settings-view.ts
+++ src/app/settings/settings-view.ts
@@ -179,22 +179,24 @@ export class SettingsView {
 					SettingsContext.getInstance().getSettings().Promode = 1;
 					SettingsContext.getInstance().getSettings().Fog = 1;
 					SettingsContext.getInstance().getSettings().Diplomacy.option = 1;
-					SettingsContext.getInstance().getSettings().Overtime.option = 0;
+					SettingsContext.getInstance().getSettings().Overtime.option = 3;
 
 					BlzFrameSetValue(fogFrame, 1);
 					BlzFrameSetEnable(fogFrame, false);
 					BlzFrameSetValue(diploFrame, 1);
 					BlzFrameSetEnable(diploFrame, false);
-					BlzFrameSetValue(overtimeFrame, 0);
+					BlzFrameSetValue(overtimeFrame, 3);
 					BlzFrameSetEnable(overtimeFrame, false);
 				} else {
 					SettingsContext.getInstance().getSettings().Promode = 0;
 					SettingsContext.getInstance().getSettings().Fog = 0;
 					SettingsContext.getInstance().getSettings().Diplomacy.option = 0;
+					SettingsContext.getInstance().getSettings().Overtime.option = 0;
 					BlzFrameSetValue(fogFrame, 0);
 					BlzFrameSetEnable(fogFrame, true);
 					BlzFrameSetValue(diploFrame, 0);
 					BlzFrameSetEnable(diploFrame, true);
+					BlzFrameSetValue(overtimeFrame, 0);
 					BlzFrameSetEnable(overtimeFrame, true);
 				}
 
